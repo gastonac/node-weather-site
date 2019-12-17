76473cf1858284d0dad5b780062579f63830ec71
gastonac <gaston.calverley@yahoo.co.za>
2019-12-17 18:53:00 +0200

    Added next day forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,21 +10,10 @@ const forecast = (latitude, longitude, callback) => {
       callback('Unable to find location. Try different coordinates.', undefined)
     } else {
       callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature +
-        ' degrees outside. There is a ' + body.currently.precipProbability * 100 + '% chance of rain today.')
+        ' degrees outside. There is a ' + body.currently.precipProbability * 100 +
+        '% chance of rain today. The summary for tomorrow: ' + body.daily.data[1].summary)
     }
   })
-
-  // Original - not destructured
-  /* request({ url: url, json: true }, (error, response) => {
-    if (error) {
-      callback('Unable to connect to weather service.', undefined)
-    } else if (response.body.error) {
-      callback('Unable to find location. Try different coordinates.', undefined)
-    } else {
-      callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature +
-        ' degrees outside. There is a ' + response.body.currently.precipProbability * 100 + '% chance of rain today.')
-    }
-  }) */
 }
 
 module.exports = forecast
