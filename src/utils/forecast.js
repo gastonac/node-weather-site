c86b204ef41b546ea581f840726ffb9422a30bc9
gastonac <gaston.calverley@yahoo.co.za>
2020-11-30 13:55:57 +0200

    Updated to the wetherstack API

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,21 +1,39 @@
-const request = require('request')
+const request = require("request");
 
 const forecast = (latitude, longitude, callback) => {
-  const url = 'https://api.darksky.net/forecast/e8c1ffc10180adf8dc44d6628f001d29/' + latitude + ',' + longitude + '?units=si'
+  const url =
+    "http://api.weatherstack.com/current?access_key=c6743b148c9a5d4a7c69827a75055a58&query=" +
+    latitude +
+    "," +
+    longitude;
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
-      callback('Unable to connect to weather service.', undefined)
+      callback("Unable to connect to weather service.", undefined);
     } else if (body.error) {
-      callback('Unable to find location. Try different coordinates.', undefined)
+      callback(
+        "Unable to find location. Try different coordinates.",
+        undefined
+      );
     } else {
-      callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature +
-        ' degrees outside. The high today is ' + body.daily.data[0].temperatureHigh +
-        ' with a low of ' + body.daily.data[0].temperatureLow + '.' +
-        ' There is a ' + body.currently.precipProbability * 100 +
-        '% chance of rain today. The summary for tomorrow: ' + body.daily.data[1].summary)
+      callback(
+        undefined,
+        "Forecast: " +
+          body.current.weather_descriptions +
+          " It is currently " +
+          body.current.temperature +
+          " degrees outside. The high today is " +
+          body.daily.data[0].temperatureHigh +
+          " with a low of " +
+          body.daily.data[0].temperatureLow +
+          "." +
+          " There is a " +
+          body.current.precip * 100 +
+          "% chance of rain today. The summary for tomorrow: " +
+          body.daily.data[1].summary
+      );
     }
-  })
-}
+  });
+};
 
-module.exports = forecast
+module.exports = forecast;
