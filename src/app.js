9fd6587f1dd1e6aeb5392199fa1fa78c6f5d07dd
gastonac <gaston.calverley@yahoo.co.za>
2020-12-02 11:14:01 +0200

    Inserted explanitory comments

--- src/app.js
+++ src/app.js
@@ -1,106 +1,111 @@
-const path = require('path')
-const express = require('express')
-const hbs = require('hbs')
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for express config
-const publicDirPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const publicDirPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine and views location
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve - middleware
-app.use(express.static(publicDirPath))
+app.use(express.static(publicDirPath));
 
-app.get('', (req, res) => {
-  res.render('index', {
-    title: 'Weather',
-    name: 'Gaston Calverley'
-  })
-})
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Gaston Calverley",
+  });
+});
 
-app.get('/about', (req, res) => {
-  res.render('about', {
-    title: 'About Me',
-    name: 'Gaston Calverley'
-  })
-})
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Gaston Calverley",
+  });
+});
 
-app.get('/help', (req, res) => {
-  res.render('help', {
-    title: 'Help',
-    message: 'This is the page that provides help with our pages.',
-    name: 'Gaston Calverley'
-  })
-})
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    message: "This is the page that provides help with our pages.",
+    name: "Gaston Calverley",
+  });
+});
 
-app.get('/weather', (req, res) => {
+// Code using the geocode & forecast functions to fetch weather data
+app.get("/weather", (req, res) => {
+  // Query is an object - we check if it has an address property
   if (!req.query.address) {
     return res.send({
-      error: 'Please provide a location for weather forecast.'
-    })
+      error: "Please provide a location for weather forecast.",
+    });
   } else {
-    geocode(req.query.address, (error, { longitude, latitude, location } = {}) => {
-      if (error) {
-        return res.send({ error })
-      }
-
-      forecast(latitude, longitude, (error, forecastData) => {
+    geocode(
+      req.query.address,
+      (error, { longitude, latitude, location } = {}) => {
         if (error) {
-          return res.send({
-            error
-          })
+          return res.send({ error });
         }
 
-        res.send({
-          forecast: forecastData,
-          location,
-          address: req.query.address
-        })
-      })
-    })
-  }
-})
+        forecast(latitude, longitude, (error, forecastData) => {
+          if (error) {
+            return res.send({
+              error,
+            });
+          }
 
-app.get('/products', (req, res) => {
+          res.send({
+            forecast: forecastData,
+            location,
+            address: req.query.address,
+          });
+        });
+      }
+    );
+  }
+});
+
+app.get("/products", (req, res) => {
+  // We 1st make sure there is a search query item before we send product data
+  // So if no search query item exists => no results are passed
   if (!req.query.search) {
     return res.send({
-      error: 'You must provide a search term.'
-    })
+      error: "You must provide a search term.",
+    });
   }
 
-  console.log(req.query.search)
+  console.log(req.query.search);
   res.send({
-    products: []
-  })
-})
+    products: [],
+  });
+});
 
-app.get('/help/*', (req, res) => {
-  res.render('404', {
-    title: '404',
-    name: 'Gaston Calverley',
-    errorMessage: 'Help article not found.'
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Gaston Calverley",
+    errorMessage: "Help article not found.",
+  });
+});
 
-  })
-})
-
-app.get('*', (req, res) => {
-  res.render('404', {
-    title: '404',
-    name: 'Gaston Calverley',
-    errorMessage: 'Page not found'
-
-  })
-})
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Gaston Calverley",
+    errorMessage: "Page not found",
+  });
+});
 
 app.listen(port, () => {
-  console.log('Server is up on port ' + port)
-})
+  console.log("Server is up on port " + port);
+});
