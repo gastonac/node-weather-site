41e0ae8e6fbe35094822d17cff6f15a0da9fe2f6
gastonac <gaston.calverley@yahoo.co.za>
2020-12-02 14:24:11 +0200

    Added descriptive comments

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,30 @@
-console.log('Client side JavaScript file is loaded!')
+console.log("Client side JavaScript file is loaded!");
 
-const weatherForm = document.querySelector('form')
-const search = document.querySelector('input')
-const msgOne = document.querySelector('#msg-1')
-const msgTwo = document.querySelector('#msg-2')
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const msgOne = document.querySelector("#msg-1");
+const msgTwo = document.querySelector("#msg-2");
 
-weatherForm.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const location = search.value
-  msgOne.textContent = 'Loading...'
-  msgTwo.textContent = ''
+// We add an event listener - callback runs evertime event occurs
+weatherForm.addEventListener("submit", (event) => {
+  // Prevents default behaviour - stops page refresh
+  event.preventDefault();
 
-  fetch('/weather?address=' + location).then((response) => {
+  // Get the search text value
+  const location = search.value;
+
+  msgOne.textContent = "Loading...";
+  msgTwo.textContent = "";
+
+  // Weather data is fetched when form is submitted
+  fetch("/weather?address=" + location).then((response) => {
     response.json().then((data) => {
       if (data.error) {
-        msgOne.textContent = data.error
+        msgOne.textContent = data.error;
       } else {
-        msgOne.textContent = data.location
-        msgTwo.textContent = data.forecast
+        msgOne.textContent = data.location;
+        msgTwo.textContent = data.forecast;
       }
-    })
-  })
-})
+    });
+  });
+});
